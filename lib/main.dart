c9fc10b6315c04713c04952f4b65df59b37c424f
fazproject53 <fazproject53@gmail.com>
2023-06-04 10:55:32 +0300

    alaa

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _MyAppState extends State<MyApp> {
               child: widget!,
             );
           },
-          home: NavStudent(),
+          home: Login(),
         );
       },
     );
